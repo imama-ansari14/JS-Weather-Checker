fc8047f231eeceaf3da71d0fd9a4ba38ca9a9dfd
Imama Ansari <ansariimama@yahoo.com>
2025-03-17 20:53:02 +0500

    project start..

---
+++ script.js
@@ -0,0 +1,4 @@
+let =prompt("From which city in Pakistan are you? Enter the name and check the current weather")
+if(karachi){
+    document.write('<img src="images/summer.webp" alt="hot weather">')
+}
