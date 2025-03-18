beceb3a4ed5b2a15594ccbf8f2f23b7aa516bd68
Imama Ansari <ansariimama@yahoo.com>
2025-03-18 07:31:31 +0500

    the project is almost done

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-let =prompt("From which city in Pakistan are you? Enter the name and check the current weather")
-if(karachi){
-    document.write('<img src="images/summer.webp" alt="hot weather">')
+let weather =prompt("From which city in Pakistan are you? Enter the name and check the current weather")
+
+let karachi="hot"
+let lahore="sunny"
+let islamabad="cloudy"
+let naran="windy"
+let muree="rainy"
+
+if(weather== 'karachi'){
+    document.write('<img src="images/summer.webp" alt="summer"height="800px"> ')
+} else if (weather=='lahore'){
+document.write('<img src="images/sunny.webp" alt="sunny" width="100%">')
+}else if (weather=='islamabad'){
+    document.write('<img src="images/cloudy.png" alt="cloudy" width="100%">')
+}else if (weather=='naran'){
+        document.write('<img src="images/windy.avif" alt="windy" width="100%">')
+}else if (weather=='muree'){
+    document.write('<img src="images/rainy.jpg" alt="rainy" width="100%">')
+    }
+else {
+
 }
