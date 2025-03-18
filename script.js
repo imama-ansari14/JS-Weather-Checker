1817633b76c6d907a50405cfd7f9b1bd3e02f363
Imama Ansari <ansariimama@yahoo.com>
2025-03-18 08:04:04 +0500

    Assignment Completed...

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let lahore="sunny"
 let islamabad="cloudy"
 let naran="windy"
 let muree="rainy"
+let kashmir="spring"
 
 if(weather== 'karachi'){
     document.write('<img src="images/summer.webp" alt="summer"height="800px"> ')
@@ -16,7 +17,9 @@ document.write('<img src="images/sunny.webp" alt="sunny" width="100%">')
         document.write('<img src="images/windy.avif" alt="windy" width="100%">')
 }else if (weather=='muree'){
     document.write('<img src="images/rainy.jpg" alt="rainy" width="100%">')
-    }
-else {
-
+}else if (weather=='kashmir'){
+        document.write('<img src="images/kashmir.jpg" alt="kashmir" width="100%">')
+}else {
+document.write(`<h1 style="text-align: center; text-decoration:5px rgb(164, 36, 36) underline;">Cant Understand, try another city </h1>
+ <img src="images/sorry.jpeg" alt="sorry" style="display: block; margin: 0 auto;">`)
 }
